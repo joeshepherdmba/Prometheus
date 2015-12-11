de4d3d137ea29eb00f5806b9d315b284eb331e1d
joeshepherdmba <joe@prometheustec.com>
2015-12-11 14:31:36 -0700

    Initial Build

---
+++ Prometheus.Web.Generics/Http/GenericHttpClientRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prometheus.Web.Generics.Http
+{
+    public class GenericHttpClientRepository<T> : IGenericHttpClientRepository<T> where T : class
+    {
+        private readonly string _baseAddress;
+        private readonly string _token; //TODO: change type to token model
+
+        public Task<bool> DeleteAsync(string apiUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T[]>> GetAllAsync(string apiUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(string apiUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> InsertAsync(string apiUrl, T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T[]>> SearchForAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(string apiUrl, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Prometheus.Web.Generics/Http/IGenericHttpClientRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prometheus.Web.Generics.Http
+{
+    public interface IGenericHttpClientRepository<T>
+    {
+        Task<T> InsertAsync(string apiUrl, T entity);
+        Task<bool> DeleteAsync(string apiUrl);
+        Task<IQueryable<T[]>> SearchForAsync(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T[]>> GetAllAsync(string apiUrl);
+        Task<T> GetByIdAsync(string apiUrl);
+        Task<bool> UpdateAsync(string apiUrl, T entity);
+    }
+}
