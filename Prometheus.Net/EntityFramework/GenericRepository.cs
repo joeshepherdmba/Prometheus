a55797ea045d646bcbefbc4cdb99b30ea2ea9718
joeshepherdmba <joe@prometheustec.com>
2015-12-12 14:13:28 -0700

    Added .Net project

---
+++ Prometheus.Net/EntityFramework/GenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prometheus.Net.EntityFramework
+{
+    public class GenericRepository<T> : IDisposable, IGenericRepository<T> where T : class
+    {
+        protected DbSet<T> DbSet;
+        protected DbContext context;
+
+        public GenericRepository(DbContext dataContext)
+        {
+            context = dataContext;
+            DbSet = dataContext.Set<T>();
+        }
+
+        public T Insert(T entity)
+        {
+            return DbSet.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate)
+        {
+            return DbSet.Where(predicate);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return DbSet;
+        }
+
+        public T GetById(object id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            context.Entry(entity).State = EntityState.Modified;
+            this.SaveChanges();
+        }
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
---
+++ Prometheus.Net/EntityFramework/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prometheus.Net.EntityFramework
+{
+    /// <summary>
+    /// Generic Repository Interface modeled from: http://www.remondo.net/repository-pattern-example-csharp/
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IGenericRepository<T>
+    {
+        T Insert(T entity);
+        void Delete(T entity);
+        IQueryable<T> SearchFor(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> GetAll();
+        T GetById(object id);
+        void Update(T entity);
+        void SaveChanges();
+    }
+}
